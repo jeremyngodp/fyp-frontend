77a64cea93bf133212fad1ed9a3c9d1fdc1df091
Jeremy Ngo <44996654+jeremyngodp@users.noreply.github.com>
2020-12-03 02:27:18 +0800

    Set up login store, action and reducers

---
+++ src/redux/login-store/reducers/authReducers.js
@@ -0,0 +1,73 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+    token: null,
+    error: null,
+    loading: false,//what is it used for??
+    user: null,
+    // projects: null,
+    is_Staff: null,
+    // paramQuery: null
+}
+
+const authStart = (state, action) => {
+    return {
+        ...state, //the state is copied and the new value is made on the copy, not directly on the state
+        error: null,
+        loading: true,
+        user: {},
+        projects: [],
+        is_Staff: null,
+        // paramQuery: null
+    }
+}
+
+const authSuccess = (state, action) => {
+    return {
+        ...state,
+        token: action.token,
+        error: null,
+        loading: false,
+        user: action.user,
+        projects: action.projects,
+        is_Staff: action.is_Staff,
+        // paramQuery: action.paramQuery
+    }
+}
+
+const authFail = (state, action) => {
+    return {
+        ...state,
+        error: action.error,
+        loading: false,
+        user: null,
+        projects: null,
+        is_Staff: null,
+        // paramQuery: null
+    }
+}
+
+const authLogout = (state, action) => {
+    return {
+        ...state,
+        token: null,
+        user: null,
+        projects: null,
+        is_Staff: null,
+        // paramQuery: null
+    }
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'AUTH_START': return authStart(state, action);
+        case 'AUTH_SUCCESS': return authSuccess(state, action);
+        case 'AUTH_FAIL': return authFail(state, action);
+        case 'AUTH_LOGOUT': return authLogout(state, action);
+        // case 'TASK_PARAMS': return tasklistParam(state, action);
+        default:
+            return state;
+    }
+}
+
+export default reducer;
